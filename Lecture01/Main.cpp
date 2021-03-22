cc93f61d263d182391eea3f49bee13d5978efee4
Konstantin Kuritsyn <kuritsyn@yoomoney.ru>
2021-03-22 10:10:52 +0300

    #2 Функция умножения двух векторов
    Векторы без инкапсуляции.
    Проверка входных параметров и выброс исключения.

--- Lecture01/Main.cpp
+++ Lecture01/Main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 
+// vector размерности size с целочисленными координатами
+int* data1;
+int size1;
+
+int* data2;
+int size2;
+
+int mult(int* data1, int size1,
+	int* data2, int size2) {
+	// всегда проверяем корректность входных параметров (для публичных функций)
+	if (size1 != size2) {
+		throw "Размеры векторов должны совпадать";
+	}
+
+
+
+	// выполняется скалярное произведение двух векторов
+	return true;
+}
+
+
 int main() {
 
 	std::cout << "Hello world" << std::endl;
